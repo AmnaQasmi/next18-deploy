473caeaad599ed3b4421af10dd81bc24f1bc3410
Amna_Qasmi <150395335+AmnaQasmi@users.noreply.github.com>
2024-07-23 15:59:23 +0500

    Fixed Finallyy

--- src/app/api/product/route.ts
+++ src/app/api/product/route.ts
@@ -1,21 +1,19 @@
+
 import { NextRequest, NextResponse } from 'next/server';
 import { createClient } from 'next-sanity';
-import { SanityClient } from 'sanity';
-import * as fs from 'fs';
-import toast from 'react-hot-toast';
 import Stripe from 'stripe';
 
 // Initialize Sanity client
-let client: SanityClient = createClient({
-  projectId: `${process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}`,
-  dataset: "production",
-  apiVersion: "2024-06-27",
+const client = createClient({
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || '',
+  dataset: 'production',
+  apiVersion: '2024-06-27',
   useCdn: true
 });
 
 // Initialize Stripe
-const stripe = new Stripe('sk_test_51Pf5EhCdXu8yBChALVGi9nzI26wGvd549P8gm8gMdguqc3vbnJd2ywGeu6b9XRvYhuiMy83FPlWYclNVlXhWBPtI0058EnbfMj', {
-  apiVersion: "2024-06-20"
+const stripe = new Stripe('sk_test_51Pf5EhCdXu8yBChALVGi9nzI26wGvd549P8gm8gMdguqc3vbnJd2ywGeu6b9XRvYhuiMy83FPlWYclNVlXhWBPtI0058EnbfMj' || '', {
+  apiVersion: '2024-06-20'
 });
 
 // Handle POST requests (Create Stripe Checkout Session)
@@ -27,7 +25,7 @@ export async function POST(req: NextRequest) {
     // Create a new Stripe Checkout Session
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ['card'],
-      line_items: body.map((item: { name: string, price: number, quantity: number}) => ({
+      line_items: body.map((item: { name: string, price: number, quantity: number }) => ({
         price_data: {
           currency: 'usd',
           product_data: { name: item.name },
@@ -53,32 +51,21 @@ export async function PUT() {
   try {
     // Fetch data from Sanity
     let response = await client.fetch(`*[_type == "Product"]{
-      title,
-      price,
-      _id,
-      details,
-      description,
-      image,
-      category -> {
-        name
-      }
-    }`);
+        title,
+        price,
+        _id,
+        details,
+        description,
+        image,
+        category -> {
+          name
+        }
+      }`);
 
     // Respond with the fetched data
     return NextResponse.json(response);
   } catch (error) {
     console.error((error as { message: string }).message);
     return NextResponse.json({ error: "Error occurred while processing PUT request" });
-  }
-}
-
-// Main handler function
-export default async function handler(req: NextRequest, res: NextResponse) {
-  if (req.method === 'POST') {
-    return POST(req);
-  } else if (req.method === 'PUT') {
-    return PUT();
-  } else {
-    return NextResponse.json({ error: "Method not allowed" }, { status: 405 });
   }
 }
