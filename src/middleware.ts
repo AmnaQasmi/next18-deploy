acc383a40163bcab7a0ddc28bb3f5a7da6d2651d
Amna_Qasmi <150395335+AmnaQasmi@users.noreply.github.com>
2024-07-23 16:53:37 +0500

    Fixed Final3

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,9 +2,9 @@ import { NextRequest, NextResponse } from 'next/server'
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-        if(request.nextUrl.pathname!='/service'){
-    return NextResponse.redirect(new URL('/service', request.url))
-    }
+    //     if(request.nextUrl.pathname!='/service'){
+    // return NextResponse.redirect(new URL('/service', request.url))
+    // }
 }
 
 // See "Matching Paths" below to learn more
