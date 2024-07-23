68599deb0760dbcad4e3333b1ff3552ff166dea2
Amna_Qasmi <150395335+AmnaQasmi@users.noreply.github.com>
2024-07-23 17:26:42 +0500

    Fixed Final4

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,13 @@
-import { NextRequest, NextResponse } from 'next/server'
+// import { NextRequest, NextResponse } from 'next/server'
 
-// This function can be marked `async` if using `await` inside
-export function middleware(request: NextRequest) {
-    //     if(request.nextUrl.pathname!='/service'){
-    // return NextResponse.redirect(new URL('/service', request.url))
-    // }
-}
+// // This function can be marked `async` if using `await` inside
+// export function middleware(request: NextRequest) {
+//     //     if(request.nextUrl.pathname!='/service'){
+//     // return NextResponse.redirect(new URL('/service', request.url))
+//     // }
+// }
 
-// See "Matching Paths" below to learn more
-export const config = {
-    matcher: '/login/:path*',
-}
+// // See "Matching Paths" below to learn more
+// export const config = {
+//     matcher: '/login/:path*',
+// }
